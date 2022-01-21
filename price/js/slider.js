3d2705eaba05d4bb419caf10eb7ed15e56cbb13a
wellerman04 <97841423+wellerman04@users.noreply.github.com>
2022-01-21 17:45:52 +0200

    Add files via upload

--- price/js/slider.js
+++ price/js/slider.js
@@ -3,6 +3,7 @@ const prev = document.querySelectorAll('.prev');
 const next = document.querySelectorAll('.next');
 const slideActive = document.getElementById('slideActive');
 const hide = document.getElementById('hide');
+let last = document.getElementById('last');
 
 
 function slider(slide){
@@ -10,16 +11,24 @@ function slider(slide){
     if(slide > 0){
         show.style.marginLeft = '-100vw';
         slideActive.innerHTML = `<p class="slideActive" id="slideActive">02</p>`;
-        hide.classList.add('show');
+        hide.classList.add('arr');
+        hide.classList.remove('hide');
+
+        last.classList.add('arr');
+        last.classList.remove('hide');
     }
     if(slide > 1){
         show.style.marginLeft = '-200vw';
         slideActive.innerHTML = `<p class="slideActive" id="slideActive">03</p>`
+        last.classList.remove('arr');
+        last.classList.add('hide');
     }
     
     if(slide == 0){
         show.style.marginLeft = '0';
         slideActive.innerHTML = `<p class="slideActive" id="slideActive">01</p>`
+        hide.classList.remove('arr');
+        hide.classList.add('hide');
     }
     
      //alert(slide);
