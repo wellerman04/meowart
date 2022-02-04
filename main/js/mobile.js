df31a65a6cf8df6162a8b6e9edd175ab37b5ee1f
wellerman04 <97841423+wellerman04@users.noreply.github.com>
2022-02-05 01:11:34 +0200

    Add files via upload

--- main/js/mobile.js
+++ main/js/mobile.js
@@ -1,8 +1,10 @@
 let burger = document.getElementById('burger');
 let burgerBtn = document.getElementById('burgerBtn');
 let burgerScreen = document.getElementById('burgerScreen');
+let wrapper = document.getElementById('wrapper');
 
 burger.addEventListener("click", function(){
     burgerBtn.classList.toggle('close');
     burgerScreen.classList.toggle('show');
+    wrapper.classList.toggle('block');
 });
