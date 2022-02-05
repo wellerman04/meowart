4ddc919bcf9069f9f12534ee51cc9f984dcc7d19
wellerman04 <97841423+wellerman04@users.noreply.github.com>
2022-02-05 15:48:26 +0200

    Delete main/#Source/scss/login directory

--- main/#Source/scss/login/js/script.js
+++
@@ -1,7 +0,0 @@
-let userIcons = document.getElementById('userIcons');
-let userItems = document.getElementById('userItems');
-
-userIcons.addEventListener('click', function(){
-    userItems.classList.toggle('active');
-    //alert('Why am I not work?')
-});
